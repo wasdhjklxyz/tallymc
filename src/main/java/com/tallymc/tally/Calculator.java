dad6a2f2cec109bc77495f2aceadfc9db26a1029
uiop <uiop@wasdhjkl.xyz>
2026-05-20 20:26:20 -0400

    feat(tally): add calculator

---
+++ src/main/java/com/tallymc/tally/Calculator.java
@@ -0,0 +1,20 @@
+package com.tallymc.tally;
+
+import org.bukkit.entity.Player;
+
+public class Calculator {
+  public static Result(double tally, double miningFrac, double combatFrac,
+                       double explorationFrac, double survivalFrac,
+                       double advancementFrac) {}
+
+  private static Result compute(Player player) {
+    double tally = 0.0;
+    double miningFrac = 0.0;
+    double combatFrac = 0.0;
+    double explorationFrac = 0.0;
+    double survivalFrac = 0.0;
+    double advancementFrac = 0.0;
+    return new Result(tally, miningFrac, combatFrac, explorationFrac,
+                      survivalFrac, advancementFrac);
+  }
+}
