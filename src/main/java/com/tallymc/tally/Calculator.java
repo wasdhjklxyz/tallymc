44b187601558fc024ac9676287b7b4f998a79449
uiop <uiop@wasdhjkl.xyz>
2026-05-20 20:44:58 -0400

    feat(tally): calculate tally

--- src/main/java/com/tallymc/tally/Calculator.java
+++ src/main/java/com/tallymc/tally/Calculator.java
@@ -8,12 +8,18 @@ public class Calculator {
                        double advancementFrac) {}
 
   private static Result compute(Player player) {
-    double tally = 0.0;
-    double miningFrac = 0.0;
+    double miningFrac = Mining.raw(player);
     double combatFrac = 0.0;
     double explorationFrac = 0.0;
     double survivalFrac = 0.0;
     double advancementFrac = 0.0;
+
+    double tally = 100.0 * (Weights.W_MINING      * miningFrac +
+                            Weights.W_COMBAT      * combatFrac +
+                            Weights.W_EXPLORATION * explorationFrac+
+                            Weights.W_SURVIVAL    * survivalFrac +
+                            Weights.W_ADVANCEMENT * advancementFrac);
+
     return new Result(tally, miningFrac, combatFrac, explorationFrac,
                       survivalFrac, advancementFrac);
   }
