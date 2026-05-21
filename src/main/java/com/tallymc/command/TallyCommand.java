1fd0e74ef017a992de8f3e9f8b9860cb1475b217
uiop <uiop@wasdhjkl.xyz>
2026-05-20 20:14:19 -0400

    chore(cmd): get target player

--- src/main/java/com/tallymc/command/TallyCommand.java
+++ src/main/java/com/tallymc/command/TallyCommand.java
@@ -7,6 +7,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +18,25 @@ public class TallyCommand implements CommandExecutor, TabCompleter {
   public boolean onCommand(@NotNull CommandSender sender,
                            @NotNull Command command, @NotNull String label,
                            @NotNull String[] args) {
+    Player target;
+    if (args.length == 0) {
+      if (!(sender instanceof Player p)) {
+        sender.sendMessage(Component.text(
+              "Must specify a player: /tally <player>", NamedTextColor.RED));
+        return true;
+      }
+      target = p;
+    } else {
+      target = Bukkit.getPlayerExact(args[0]);
+      if (target == null) {
+        sender.sendMessage(Component.text(
+              "Player not found or offline: " + args[0], NamedTextColor.RED));
+        return true;
+      }
+    }
+
+    target.giveExpLevels(10);
+
     sender.sendMessage("Hello, world!");
     return true;
   }
