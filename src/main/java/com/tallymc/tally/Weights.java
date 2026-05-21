36992f2d821b2f832eb8535d4cbdf38595b87499
uiop <uiop@wasdhjkl.xyz>
2026-05-20 22:46:12 -0400

    chore(tally): advancements

--- src/main/java/com/tallymc/tally/Weights.java
+++ src/main/java/com/tallymc/tally/Weights.java
@@ -89,31 +89,64 @@ public class Weights {
   }
 
   public static final Map<String, Tier> ADVANCEMENT_TIERS = Map.ofEntries(
-      Map.entry("nether/all_effects",         Tier.EXTREME),
-      Map.entry("adventure/adventuring_time", Tier.EXTREME),
-      Map.entry("husbandry/balanced_diet",    Tier.EXTREME),
-      Map.entry("end/levitate",               Tier.EXTREME),
+      Map.entry("nether/all_effects",            Tier.EXTREME), // How Did We Get Here?
+      Map.entry("nether/all_potions",            Tier.EXTREME), // A Furious Cocktail
+      Map.entry("adventure/adventuring_time",    Tier.EXTREME), // all 54 biomes
+      Map.entry("nether/explore_nether",         Tier.EXTREME), // Hot Tourist Destinations
+      Map.entry("husbandry/balanced_diet",       Tier.EXTREME), // A Balanced Diet
+      Map.entry("husbandry/bred_all_animals",    Tier.EXTREME), // Two by Two
+      Map.entry("adventure/kill_all_mobs",       Tier.EXTREME), // Monsters Hunted
+      Map.entry("adventure/overoverkill",        Tier.EXTREME), // Over-Overkill (mace, 50 hearts)
+      Map.entry("adventure/arbalistic",          Tier.EXTREME), // 5 mobs, 1 crossbow shot
 
-      Map.entry("adventure/kill_all_mobs",       Tier.HARD),
-      Map.entry("nether/create_full_beacon",     Tier.HARD),
-      Map.entry("husbandry/froglights",          Tier.HARD),
-      Map.entry("husbandry/obtain_sniffer_egg",  Tier.HARD),
-      Map.entry("adventure/blowback",            Tier.HARD),
-      Map.entry("nether/get_wither_skull",       Tier.HARD),
-      Map.entry("nether/summon_wither",          Tier.HARD),
-      Map.entry("end/kill_dragon",               Tier.HARD),
+      Map.entry("nether/create_full_beacon",     Tier.HARD),    // Beaconator
+      Map.entry("husbandry/froglights",          Tier.HARD),    // With Our Powers Combined!
+      Map.entry("husbandry/obtain_sniffer_egg",  Tier.HARD),    // Smells Interesting
+      Map.entry("husbandry/complete_catalogue",  Tier.HARD),    // A Complete Catalogue
+      Map.entry("husbandry/leash_all_frog_variants", Tier.HARD),// When the Squad Hops into Town
+      Map.entry("adventure/blowback",            Tier.HARD),    // Blowback
       Map.entry("adventure/hero_of_the_village", Tier.HARD),
-      Map.entry("husbandry/complete_catalogue",  Tier.HARD),
-      Map.entry("adventure/totem_of_undying",    Tier.HARD),
+      Map.entry("adventure/totem_of_undying",    Tier.HARD),    // Postmortal
+      Map.entry("adventure/two_birds_one_arrow", Tier.HARD),
+      Map.entry("adventure/sniper_duel",         Tier.HARD),
+      Map.entry("adventure/trim_with_all_exclusive_armor_patterns", Tier.HARD), // Smithing with Style
+      Map.entry("adventure/fall_from_world_height", Tier.HARD), // Caves & Cliffs
+      Map.entry("nether/summon_wither",          Tier.HARD),
+      Map.entry("nether/get_wither_skull",       Tier.HARD),    // Spooky Scary Skeleton
+      Map.entry("nether/uneasy_alliance",        Tier.HARD),
+      Map.entry("nether/return_to_sender",       Tier.HARD),
+      Map.entry("nether/fast_travel",            Tier.HARD),    // Subspace Bubble
+      Map.entry("end/kill_dragon",               Tier.HARD),    // Free the End
+      Map.entry("end/levitate",                  Tier.HARD),    // Great View From Up Here
+      Map.entry("end/respawn_dragon",            Tier.HARD),
 
-      Map.entry("story/enter_the_nether",          Tier.UNCOMMON),
-      Map.entry("story/follow_ender_eye",          Tier.UNCOMMON),
-      Map.entry("story/enter_the_end",             Tier.UNCOMMON),
-      Map.entry("nether/obtain_ancient_debris",    Tier.UNCOMMON),
-      Map.entry("nether/netherite_armor",          Tier.UNCOMMON),
-      Map.entry("story/mine_diamond",              Tier.UNCOMMON),
-      Map.entry("adventure/trade_at_world_height", Tier.UNCOMMON),
-      Map.entry("end/elytra",                      Tier.UNCOMMON),
-      Map.entry("end/dragon_egg",                  Tier.UNCOMMON)
+      Map.entry("story/enter_the_nether",        Tier.UNCOMMON),
+      Map.entry("story/follow_ender_eye",        Tier.UNCOMMON), // Eye Spy
+      Map.entry("story/enter_the_end",           Tier.UNCOMMON), // The End?
+      Map.entry("story/mine_diamond",            Tier.UNCOMMON), // Diamonds!
+      Map.entry("story/enchant_item",            Tier.UNCOMMON), // Enchanter
+      Map.entry("story/cure_zombie_villager",    Tier.UNCOMMON), // Zombie Doctor
+      Map.entry("story/shiny_gear",              Tier.UNCOMMON), // Cover Me with Diamonds
+      Map.entry("nether/obtain_ancient_debris",  Tier.UNCOMMON), // Hidden in the Depths
+      Map.entry("nether/netherite_armor",        Tier.UNCOMMON), // Cover Me in Debris
+      Map.entry("nether/create_beacon",          Tier.UNCOMMON), // Bring Home the Beacon
+      Map.entry("nether/obtain_blaze_rod",       Tier.UNCOMMON), // Into Fire
+      Map.entry("nether/find_bastion",           Tier.UNCOMMON), // Those Were the Days
+      Map.entry("nether/find_fortress",          Tier.UNCOMMON), // A Terrible Fortress
+      Map.entry("nether/loot_bastion",           Tier.UNCOMMON), // War Pigs
+      Map.entry("nether/charge_respawn_anchor",  Tier.UNCOMMON),
+      Map.entry("nether/distract_piglin",        Tier.UNCOMMON), // Oh Shiny
+      Map.entry("end/elytra",                    Tier.UNCOMMON), // Sky's the Limit
+      Map.entry("end/dragon_egg",                Tier.UNCOMMON), // The Next Generation
+      Map.entry("end/dragon_breath",             Tier.UNCOMMON), // You Need a Mint
+      Map.entry("end/find_end_city",             Tier.UNCOMMON),
+      Map.entry("adventure/trade_at_world_height", Tier.UNCOMMON), // Star Trader
+      Map.entry("adventure/voluntary_exile",     Tier.UNCOMMON),
+      Map.entry("adventure/salvage_sherd",       Tier.UNCOMMON), // Respecting the Remnants
+      Map.entry("adventure/craft_decorated_pot_using_only_sherds", Tier.UNCOMMON), // Careful Restoration
+      Map.entry("husbandry/silk_touch_nest",     Tier.UNCOMMON), // Total Beelocation
+      Map.entry("husbandry/obtain_netherite_hoe", Tier.UNCOMMON), // Serious Dedication
+      Map.entry("husbandry/wax_on",              Tier.UNCOMMON),
+      Map.entry("husbandry/tame_an_animal",      Tier.UNCOMMON)
   );
 }
